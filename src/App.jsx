f662af7285d701a67119f1b0b3107a031f3be8f1
Ana Picado <anapicado@Anas-MacBook-Air.local>
2022-06-24 21:45:43 -0600

    to-dos

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import React from 'react';
+
+const App = function () {
+    return(
+        <div className='App'>
+            <h1>Hello All!</h1>
+        </div>
+    );
+}
+
+export default App;
